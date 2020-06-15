3457e9c4ad27c027d2774196caa36be0f60fda04
Cal Paterson <cal@calpaterson.com>
2020-06-15 17:33:24 +0100

    Skeleton code

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+VERSION = open("VERSION").read().strip()
+
+setup(
+    name="pyappcache",
+    version=VERSION,
+    packages=find_packages(exclude=["tests.*", "tests"]),
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[],
+    extras_require={
+        "tests": ["pytest~=5.3.1",],
+        "dev": ["wheel~=0.33.6", "black~=19.10b0", "mypy~=0.750", "bpython~=0.18",],
+    },
+)
---
+++ tests/test_keys.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+@pytest.mark.xfail(reason="not implemented")
+def test_key():
+    assert False
