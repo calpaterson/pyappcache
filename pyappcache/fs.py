47af69a17b8d5f647788708f8848ffa91385ad6a
Cal Paterson <cal@calpaterson.com>
2024-03-20 10:41:18 +0200

    wip

--- pyappcache/fs.py
+++ pyappcache/fs.py
@@ -1,19 +1,41 @@
-from typing import Optional, IO
+import sqlite3
+from typing import Optional, IO, List
 from logging import getLogger
 from pathlib import Path
 import shutil
+from contextlib import closing
 
 from .cache import Cache
 
 logger = getLogger(__name__)
 
+CREATE_DDL = """
+CREATE TABLE IF NOT EXISTS pyappcache (
+    key PRIMARY KEY,
+    expiry NOT NULL,
+    last_read NOT NULL,
+    size NOT NULL
+);
+"""
+
+INDEX_DDL: List[str] = []
+
 # FIXME: respect ttls
 # FIXME: allow setting a max cache size
 class FilesystemCache(Cache):
+    METADATA_DB_FILENAME = "metadata.sqlite3"
+
     def __init__(self, directory: Path):
         super().__init__()
         self.directory = directory
         self.directory.mkdir(parents=True, exist_ok=True)
+        self.conn = sqlite3.connect(str(self.directory / "metadata.sqlite"))
+        with closing(self.conn.cursor()) as cursor:
+            cursor.execute(CREATE_DDL)
+            for index_ddl in INDEX_DDL:
+                cursor.execute(index_ddl)
+            self.conn.commit()
+
 
     def _make_path(self, raw_key: str) -> Path:
         path = self.directory / raw_key.replace("/", "_")
